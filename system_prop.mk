a44fe998ceb145f099707dd8583e4e56482ce3dd
sjllls <2012ji123@gmail.com>
2020-03-31 22:29:07 +0800

    system_prop: Add debug buff

--- system_prop.mk
+++ system_prop.mk
@@ -87,3 +87,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Vendor
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.va_aosp.support=1
+
+# Debug
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.adb.secure=0 \
+    ro.control_privapp_permissions=log
