c3ef7a20521ae76ffcf6c49a6c212aa83baa0e54
Danny Trunk <dtrunk90@gmail.com>
2023-09-14 21:53:18 +0200

    tama-common: Fix display PRODUCT_PACKAGES for 4.19

--- common.mk
+++ common.mk
@@ -191,11 +191,10 @@ PRODUCT_PACKAGES += \
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
-    android.hardware.graphics.mapper@2.0-impl-qti-display \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     gralloc.sdm845 \
-    hwcomposer.sdm845 \
     libdisplayconfig.qti \
     libdisplayconfig.system.qti \
     libqdMetaData \
@@ -204,7 +203,7 @@ PRODUCT_PACKAGES += \
     memtrack.sdm845 \
     vendor.display.config@1.0.vendor \
     vendor.display.config@2.0 \
-    vendor.qti.hardware.display.allocator@1.0-service
+    vendor.qti.hardware.display.allocator-service
 
 # DRM
 PRODUCT_PACKAGES += \
